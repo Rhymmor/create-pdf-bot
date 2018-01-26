28f1eed0398405a29e6597da4ca60ad1548bf3c1
Anatoly Belonog <anatolybelonog@gmail.com>
2018-01-26 11:55:08 +0700

    keep created pdfs in tmp directories and clean after sending

--- src/bot.ts
+++ src/bot.ts
@@ -19,7 +19,10 @@ export class CreatePdfBot {
         this.bot.command('start', this.handleStartCommand);
         this.bot.command('help', this.handleHelpCommand);
         this.bot.command('end', this.handleEndCommand);
-        this.bot.hears(CreatePdfBot.CREATE_LABEL, ctx => ctx.reply('Cool! Collecting pdf from the images above.'));
+        this.bot.hears(CreatePdfBot.CREATE_LABEL, ctx => {
+            ctx.reply('Cool! Collecting pdf from the images above.');
+            this.handleEndCommand(ctx);
+        });
     }
 
     public start() {
@@ -46,22 +49,28 @@ export class CreatePdfBot {
         return ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id;
     }
 
-    private handleEndCommand = async (ctx: any) => {
-        const images = await Promise.all(this.media);
-        const pdf = this.pdfCreator.create(images);
-        ctx.replyWithDocument({source: pdf, filename: CreatePdfBot.PDF_NAME});
+    private getUserId = (ctx: any): string => {
+        return String(ctx.update.message.from.id);
     }
 
-    private handleStartCommand(ctx: any) {
+    private handleEndCommand = async (ctx: any) => {
+        const images = await Promise.all(this.media);
+        const id = this.getUserId(ctx);
+        const pdf = await this.pdfCreator.create(id, images);
+        ctx.replyWithDocument({source: pdf, filename: CreatePdfBot.PDF_NAME});
+        this.pdfCreator.clean(id);
+    }
+
+    private handleStartCommand = (ctx: any) => {
         return ctx.reply(this.showHelp());
     }
 
-    private handleHelpCommand(ctx: any) {
+    private handleHelpCommand = (ctx: any) => {
         return ctx.reply(this.showHelp());
     }
 
     private showHelp() {
         return `Hello, stranger! I can help you to create pdf from images.
-            To do that just send images to me, wait them to upload and then click create button.`;
+        To do that just send images to me, wait them to upload and then click create button.`;
     }
 }
--- src/pdf.ts
+++ src/pdf.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as PdfKit from 'pdfkit';
 import * as fs from 'fs';
+import { TmpDirsWatcher } from './tmpDirs';
 
 // A4 size. TODO: support more pdf sizes
 const width = 595;
@@ -8,19 +9,28 @@ const height = 842;
 
 export class PdfCreator {
     private defaultName: string;
-    
+    private dirsWatcher: TmpDirsWatcher;
+
     constructor(defaultName: string) {
         this.defaultName = defaultName;
+        this.dirsWatcher = new TmpDirsWatcher();
     }
 
-    create = (photos: Buffer[]) => {
+    create = async (id: string, photos: Buffer[]) => {
+        console.log(`Creating pdf for id ${id}`);
+        const pdfDir = await this.dirsWatcher.prepareIdDir(id);
         const pdf = new PdfKit({layout: 'portrait', size: [width, height]});
-        const pdfPath = path.join(process.cwd(), this.defaultName);
+        const pdfPath = path.join(pdfDir, this.defaultName);
         pdf.pipe(fs.createWriteStream(pdfPath));
         this.addPhotosToPdf(pdf, photos);
         pdf.end();
 
+        console.log(`Created pdf for id ${id}: ${pdfPath}`);
         return pdfPath;
+    }
+
+    clean(id: string) {
+        this.dirsWatcher.clean(id);
     }
 
     private addPhotosToPdf(pdf: PDFKit.PDFDocument, photos: Buffer[]) {
