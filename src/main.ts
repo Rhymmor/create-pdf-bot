9b7189eace88843d7b2dd42b5c0c5e6afee06531
Anatoly Belonog <anatolybelonog@gmail.com>
2018-01-26 23:44:01 +0700

    set process signals handlers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { CreatePdfBot } from "./bot";
 import { logger } from "./logger";
 
-process.on('unhandledRejection', up => { throw up });
-
 const token_env = 'BOT_TOKEN';
 const token = process.env[token_env];
 
@@ -11,7 +9,23 @@ if (!token) {
 }
 
 const bot = new CreatePdfBot(token);
+
+async function killProcess(e: any) {
+    logger.warn(`process exits with '${e}'`)
+    await bot.cleanup();
+    logger.info('Cleanup completed');
+    process.exit(1);
+}
+
+function setSignalHandlers() {
+    process.on('SIGTERM', () => killProcess("SIGTERM"));
+    process.on('SIGINT', () => killProcess("SIGINT"));
+    process.on('uncaughtException', (e: any) => killProcess(e));
+    process.on('unhandledRejection', up => { throw up });
+}
+
 try {
+    setSignalHandlers();
     bot.start();
     logger.info('Started bot');
 } catch (e) {
