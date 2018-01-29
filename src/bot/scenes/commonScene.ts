d69e3b69a7666b24b30da62eaf748d7599b7fa8d
Anatoly Belonog <anatolybelonog@gmail.com>
2018-01-29 22:55:33 +0700

    add abstract scene class

---
+++ src/bot/scenes/commonScene.ts
@@ -0,0 +1,21 @@
+import Scene = require('telegraf/scenes/base');
+import { Scenes } from '../stage';
+
+export abstract class CommonScene<T extends string> {
+    protected scene: Scene;
+
+    constructor(name: Scenes, buttons: T[]) {
+        this.scene = new Scene(name);
+        this.scene.enter(this.onEnter.bind(this));
+        for (const button of buttons) {
+            this.scene.hears(button, this.onClick(button));
+        }
+    }
+
+    protected abstract onEnter(ctx: any): any;
+    protected abstract onClick(arg: T): (ctx: any) => any;
+
+    protected getUserId = (ctx: any): string => {
+        return String(ctx.update.message.from.id);
+    }
+}
