7246700685d551cc785b2794bd9f172db7faf4a6
Anatoly Belonog <anatolybelonog@gmail.com>
2018-02-18 01:11:27 +0700

    add scenes logging

--- src/bot/scenes/commonScene.ts
+++ src/bot/scenes/commonScene.ts
@@ -1,21 +1,36 @@
 import Scene = require('telegraf/scenes/base');
 import { Scenes } from '../stage';
+import { logger } from '../../logger';
 
 export abstract class CommonScene<T extends string> {
     protected scene: Scene;
 
     constructor(name: Scenes, buttons?: T[]) {
         this.scene = new Scene(name);
-        this.scene.enter(this.onEnter.bind(this));
+        this.scene.enter(this.onEnterLogged(name));
         if (buttons) {
             for (const button of buttons) {
-                this.scene.hears(button, this.onClick(button));
+                this.scene.hears(button, this.onClickLogged(button));
             }
+        }
+    }
+    
+    private onClickLogged = (button: T) => {
+        return (ctx: any) => {
+            logger.debug(`User ${this.getUserId(ctx)} clicked '${button}' button`);
+            this.onClick(button)(ctx);
+        }
+    }
+
+    private onEnterLogged = (name: Scenes): any => {
+        return (ctx: any) => {
+            logger.debug(`User ${this.getUserId(ctx)} entered '${name}' scene`);
+            this.onEnter(ctx);
         }
     }
 
     protected abstract onEnter(ctx: any): any;
-    protected abstract onClick(arg: T): (ctx: any) => any;
+    protected abstract onClick(button: T): (ctx: any) => any;
 
     protected getUserId = (ctx: any): string => {
         return String(ctx.update.message.from.id);
